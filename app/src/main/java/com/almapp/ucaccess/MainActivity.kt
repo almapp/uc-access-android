cf807b67ad9c5a8f244532a4ec00d41d683b37e0
Patricio López <lopezjuripatricio@gmail.com>
2016-02-05 12:09:03 -0300

    add app style

--- app/src/main/java/com/almapp/ucaccess/MainActivity.kt
+++ app/src/main/java/com/almapp/ucaccess/MainActivity.kt
@@ -2,11 +2,12 @@ package com.almapp.ucaccess
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.Toolbar
-
+import com.mikepenz.ionicons_typeface_library.Ionicons
+import com.mikepenz.materialdrawer.AccountHeaderBuilder
 import com.mikepenz.materialdrawer.Drawer
 import com.mikepenz.materialdrawer.DrawerBuilder
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -17,14 +18,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Setup toolbar
         setSupportActionBar(this.toolbar)
 
+        // Build navigation drawer
         this.result = DrawerBuilder(this)
                 .withRootView(this.drawer_container)
                 .withToolbar(toolbar)
+                .withAccountHeader(
+                        AccountHeaderBuilder()
+                                .withActivity(this)
+                                .withHeaderBackground(R.drawable.header)
+                                .addProfiles(
+                                        ProfileDrawerItem().withName("Accesos UC").withEmail("username@uc.cl").withIcon(R.drawable.icon)
+                                )
+                                .build()
+
+                )
                 .withActionBarDrawerToggleAnimated(true)
                 .addDrawerItems(
-                    PrimaryDrawerItem().withName("Hola"))
+                        PrimaryDrawerItem().withName("Páginas").withIcon(Ionicons.Icon.ion_ios_book_outline),
+                        PrimaryDrawerItem().withName("Cuentas").withIcon(Ionicons.Icon.ion_android_person)
+                )
+                .addStickyDrawerItems(PrimaryDrawerItem().withName("Información").withIcon(Ionicons.Icon.ion_information_circled))
                 .withSavedInstance(savedInstanceState)
                 .build()
     }
